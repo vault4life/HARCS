7777ac0e490594af5f6c6bf8159ee3bc45cee00a
vault4life <andrewsmith2@letu.edu>
2014-06-24 15:06:46 -0400

    initial commit comment

---
+++ gen/com/collaborationlabs/harcshelparandomcollegestudent/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.collaborationlabs.harcshelparandomcollegestudent;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
